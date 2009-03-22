83d8a100612dd74b7bdebbc5197ff3713cc6d541
Randy Walker <randy@walkersystems.net>
2009-03-22 16:26:24 -0700

    added my name to readme & small changes to forgot.rb

--- lib/authorization_add_ons/forgot.rb
+++ lib/authorization_add_ons/forgot.rb
@@ -1,1 +1,23 @@
-# RaAddOns
+module AuthorizationAddOns
+  module Forgot
+    
+    def self.included( recipient )
+      recipient.extend( StatefulRolesClassMethods )
+      recipient.class_eval do
+        include StatefulRolesInstanceMethods
+      end
+    end
+
+    module StatefulRolesClassMethods
+    end # class methods
+
+    module StatefulRolesInstanceMethods
+      def recently_forgot_password?
+        @forgotten_password
+      end
+      def recently_reset_password?
+        @reset_password
+      end
+    end # instance methods
+  end
+end
