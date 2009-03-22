db72b855e5df6c59946c94785b8924519313ab4a
Randy Walker <randy@walkersystems.net>
2009-03-22 16:35:41 -0700

    moved local repository and added make_password_reset_code to forgot.rb

--- lib/authorization_add_ons/forgot.rb
+++ lib/authorization_add_ons/forgot.rb
@@ -26,8 +26,17 @@ module AuthorizationAddOns
       def recently_forgot_password?
         @forgotten_password
       end
+      
       def recently_reset_password?
         @reset_password
+      end
+      
+      def self.find_for_forget(email)
+        find :first, :conditions => ['email = ? and activated_at IS NOT NULL', email]
+      end
+      
+      def make_password_reset_code
+        self.password_reset_code = Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )
       end
     end # instance methods
   end
